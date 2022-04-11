3b57c712873cdfbd59950ae76c9b9dba1c399ab1
kavinteng <76694895+kavinteng@users.noreply.github.com>
2022-04-11 16:24:32 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -7,19 +7,19 @@ import torch
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
-file_name = None
 model = None
+
+if model == None:
+    print('start load model!!!')
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True, device='cpu')
+    model.conf = 0.5
+    model.iou = 0.4
+    file_name = None
+    print('load yolov5 successfully!!!')
 
 @app.route('/count_person', methods=['GET', 'POST'])
 def form_example():
-    global file_name, Date, Time, date_img, model
-
-    if model == None:
-        print('start load model!!!')
-        model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True, device='cpu')
-        model.conf = 0.5
-        model.iou = 0.4
-        print('load yolov5 successfully!!!')
+    global file_name, Date, Time, date_img
 
     if request.is_json:
         result2 = request.get_json()
